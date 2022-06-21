8d3c50b4464984540916cf50ceaaddabccdfc93c
darrenmcc <darren.rmc@gmail.com>
2022-06-21 13:46:10 -0400

    Look for main.nf

--- upload.go
+++ upload.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 )
 
+var mainFound bool
+
 func Exec(repoDir string) error {
 	err := filepath.Walk(repoDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -17,10 +19,15 @@ func Exec(repoDir string) error {
 		if strings.Contains(path, "/workflows/") {
 			fmt.Println(path)
 		}
+		mainFound = mainFound || info.Name() == "main.nf"
 
 		return nil
 	})
 	if err != nil {
 		log.Fatalf("unable to walk current dir: %s", err)
+	}
+
+	if !mainFound {
+		log.Fatalln("main.nf not found")
 	}
 }
